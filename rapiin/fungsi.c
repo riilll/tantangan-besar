04740b84d1cfd7e82a985f65672e8c03d56e2ec0
fahrilbelajar <fahril56@mhs.usk.ac.id>
2025-12-20 19:48:29 +0700

    ini update lagi

--- rapiin/fungsi.c
+++ rapiin/fungsi.c
@@ -1,7 +1,7 @@
 #include <string.h>
 #include <stdio.h>
 #include <ctype.h>
-#include "fungsi.h"
+#include "header.h"
 
 huruf kelompok[26];
 
--- rapiin/main.c
+++ rapiin/main.c
@@ -1,5 +1,5 @@
 #include <stdio.h>
-#include "fungsi.h"
+#include "header.h"
  
 int main() {
     int pilihan;
